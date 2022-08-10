16155d3ee6d877e6cf473e32c29f55a32d724246
Mateusz Melaniuk <mateusz.melaniuk98@gmail.com>
2022-08-10 15:36:50 +0200

    Create Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import Home from "./pages/Home";
 import SingleProduct from "./pages/SingleProduct";
 import ProductList from "./pages/ProductList";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-  return <Home />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products/:category" element={<ProductList />} />
+        <Route path="/product/:id" element={<SingleProduct />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
